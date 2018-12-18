2a6526a93c3caf15fda3864d51b4dddf00e0b6ce
danaschemer <amahendra7@gmail.com>
2018-12-18 10:13:56 +0700

    Add files via upload

---
+++ coeg.php
@@ -0,0 +1,6 @@
+<?php
+$ids = '14308';
+//pake ssl capture http://saosdeveloper.club/mtc/loadbalance.php?id=[yang ini coeg]
+//gabung di group telegram
+//https://t.me/joinchat/Jogsswve501iToc68kzo8w
+?>
